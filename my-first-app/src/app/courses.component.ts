775d45fbd5d456d7a45896302e8c5e4232cc4abd
shocklaing <shockwater@gmail.com>
2017-09-24 15:54:28 +0800

    Using template to replace component title

--- my-first-app/src/app/courses.component.ts
+++ my-first-app/src/app/courses.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses', // css selector or customerize html markup
-    template: '<h2>Courses</h2>'
+    template: '<h2>{{ getTitle() }}</h2>'
 
 })
 
 export class CoursesCompoent {
+    title = "List of courses";
 
+    getTitle() {
+        return this.title;
+    }
 }
