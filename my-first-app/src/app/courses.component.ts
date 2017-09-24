dd614266ce7983711396929049c610b6ae76a76b
shocklaing <shockwater@gmail.com>
2017-09-24 15:59:42 +0800

    Directives exercise

--- my-first-app/src/app/courses.component.ts
+++ my-first-app/src/app/courses.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses', // css selector or customerize html markup
-    template: '<h2>{{ getTitle() }}</h2>'
+    template: `
+        <h2>{{ title }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+    `
 
 })
 
 export class CoursesCompoent {
     title = "List of courses";
-
-    getTitle() {
-        return this.title;
-    }
+    courses = ["couse1", "course2", "course3"];
 }
