8a4c4d2ec7b5dbc6d620131bd8a9aa6796b14a14
shocklaing <shockwater@gmail.com>
2017-09-24 15:38:48 +0800

    First component exercise

---
+++ my-first-app/src/app/courses.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'courses', // css selector or customerize html markup
+    template: '<h2>Courses</h2>'
+
+})
+
+export class CoursesCompoent {
+
+}
