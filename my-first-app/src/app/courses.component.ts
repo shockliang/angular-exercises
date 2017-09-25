5c39401bdfebba8e85171d89678488865dc7ce43
shocklaing <shockwater@gmail.com>
2017-09-25 22:51:46 +0800

    Attribute binding exercise

--- my-first-app/src/app/courses.component.ts
+++ my-first-app/src/app/courses.component.ts
@@ -4,11 +4,12 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses', // css selector or customerize html markup
     template: `
-        <h2>{{ title }}</h2>
-        <h2>[textContent]="title"></h2>
-        
-        <img src="{{ imageUrl }}" />
         <img [src]="imageUrl" />
+        <table>
+            <tr>
+                <td [attr.colspan]="colSpan"></td>
+            </tr>
+        </table>
     `
 
 })
