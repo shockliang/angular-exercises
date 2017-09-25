c8766382c5b56e5ecc93db5772d8d6aa0a6b74dd
shocklaing <shockwater@gmail.com>
2017-09-25 23:00:32 +0800

    Add bootstrap

--- my-first-app/src/app/courses.component.ts
+++ my-first-app/src/app/courses.component.ts
@@ -4,12 +4,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses', // css selector or customerize html markup
     template: `
-        <img [src]="imageUrl" />
-        <table>
-            <tr>
-                <td [attr.colspan]="colSpan"></td>
-            </tr>
-        </table>
+        <button class="btn btn-primary">Save</button>
     `
 
 })
