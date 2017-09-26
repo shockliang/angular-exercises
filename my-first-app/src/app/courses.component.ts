cf934786449f9d1b3caae81c274d43f0fd0d6f82
shocklaing <shockwater@gmail.com>
2017-09-26 22:16:52 +0800

    Pipes

--- my-first-app/src/app/courses.component.ts
+++ my-first-app/src/app/courses.component.ts
@@ -3,14 +3,20 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses', // css selector or customerize html markup
     template: `
-        <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
+        {{ course.title | uppercase | lowercase}} <br/>
+        {{ course.students | number }} <br/>
+        {{ course.rating | number:'2.1-1'}} <br/>
+        {{ course.price | currency:'AUD':true:'3.1-1' }} <br/>
+        {{ course.releaseDate | date:'shortDate' }}
     `
 })
 
 export class CoursesCompoent {
-    email = "abc@abc.com"
-
-    onKeyUp() {
-        console.log(this.email);
+    course = {
+        title: "The Complete Angular Course",
+        rating: 4.9745,
+        students: 30123,
+        price: 190.95,
+        releaseDate: new Date(2017, 9, 26)
     }
 }
