bcb77190a26cb1697d415ea3015c2af6933e3fa8
shocklaing <shockwater@gmail.com>
2017-09-26 21:31:52 +0800

    Style binding

--- my-first-app/src/app/courses.component.ts
+++ my-first-app/src/app/courses.component.ts
@@ -4,19 +4,11 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses', // css selector or customerize html markup
     template: `
-        <button class="btn btn-primary" [class.active]="isActive">Save</button>
+        <button [style.backgroundColor]="isActive ? 'blue' : 'white' ">Save</button>
     `
-
 })
 
 export class CoursesCompoent {
-    title = "List of courses";
     isActive = true;
-    courses;
-
-    constructor(service: CoursesService) {
-        this.courses = service.getCourses();
-        
-    }
     
 }
