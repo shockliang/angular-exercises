5a2a59877929a39834784718f6c066d0d1fd46ba
shocklaing <shockwater@gmail.com>
2017-10-04 17:35:03 +0800

    Favorite component exercise

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesCompoent,
     CourseComponent,
-    SummaryPipe
+    SummaryPipe,
+    FavoriteComponent
   ],
   imports: [
     BrowserModule,
