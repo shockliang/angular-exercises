c04c30a76dbddd4dce9f21cbdbf70cf7826cdced
shocklaing <shockwater@gmail.com>
2017-10-05 23:44:40 +0800

    Input properties exerciese

--- my-first-app/src/app/favorite/favorite.component.ts
+++ my-first-app/src/app/favorite/favorite.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+//import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
+  //inputs: ['isFavorite']
 })
 export class FavoriteComponent implements OnInit {
-  isFavorite: boolean;
+  @Input() isFavorite: boolean;
+  //isFavorite: boolean;
 
   constructor() { }
 
