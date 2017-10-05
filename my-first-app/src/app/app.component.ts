c8e9aadd0ff0c8973620efed4aa7abceaecf9515
shocklaing <shockwater@gmail.com>
2017-10-05 23:59:55 +0800

    Output properties exercise

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -5,9 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   post = {
     title: "Title",
     isFavorite: true,
+  }
+
+  onFavoriteChanged() {
+    console.log("Favorite changed");
   }
 }
