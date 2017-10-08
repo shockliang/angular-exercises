e08407e7c22cf2281fa369faae6fa69716b8e318
shocklaing <shockwater@gmail.com>
2017-10-08 23:18:29 +0800

    ngFor and change detection exercise

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -8,8 +8,21 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   courses = [
-    { id: 1, name: 'course1'},
-    { id: 2, name: 'course2'},
-    { id: 3, name: 'course3'},
-  ]
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' },
+  ];
+
+  onAdd() {
+    this.courses.push({ id: 4, name: 'course4' });
+  }
+
+  onRemove(course) {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
+
+  onChange(course) {
+    course.name = 'UPDATE'
+  }
 }
