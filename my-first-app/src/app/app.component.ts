2783c703b79fb2a86fc44c60069c28d811f30b12
shocklaing <shockwater@gmail.com>
2017-10-08 23:11:25 +0800

    ngFor directive exercise

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  viewMode = 'map';
-  
+  courses = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 3, name: 'course3'},
+  ]
 }
