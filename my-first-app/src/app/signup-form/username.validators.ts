009886e5bbe1fb1a1ebdc0afed50c69062705e09
Shock Liang <shockwater@gmail.com>
2017-10-22 00:38:54 +0800

    Asynchronous validators exercise

--- my-first-app/src/app/signup-form/username.validators.ts
+++ my-first-app/src/app/signup-form/username.validators.ts
@@ -1,11 +1,23 @@
 import { AbstractControl, ValidationErrors } from "@angular/forms";
 
 export class UsernameValidators {
-    static cannotContainSpace(control: AbstractControl) : ValidationErrors | null {
-        if((control.value as string).indexOf(' ') >= 0) {
+    static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
+        if ((control.value as string).indexOf(' ') >= 0) {
             return { cannotContainSpace: true };
         }
 
         return null;
+    }
+
+    static shouldBeUnique(control: AbstractControl): Promise<ValidationErrors|null> {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                // console.log('Ok');
+                if (control.value === 'shock')
+                    resolve({ shouldBeUnique: true });
+                else 
+                    resolve(null);
+            }, 2000);
+        });
     }
 }
