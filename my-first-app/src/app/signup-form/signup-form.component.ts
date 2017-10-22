13f81a4b66a9ffbea32193f7ba6b65de40c90168
Shock Liang <shockwater@gmail.com>
2017-10-22 16:08:12 +0800

    Validating the form input upon submit

--- my-first-app/src/app/signup-form/signup-form.component.ts
+++ my-first-app/src/app/signup-form/signup-form.component.ts
@@ -10,7 +10,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 export class SignupFormComponent {
   form = new FormGroup({
-   username: new FormControl('', [
+    username: new FormControl('', [
       Validators.required,
       Validators.minLength(3),
       UsernameValidators.cannotContainSpace
@@ -18,6 +18,12 @@ export class SignupFormComponent {
       UsernameValidators.shouldBeUnique),
     password: new FormControl('', Validators.required)
   });
+
+  login() {
+    this.form.setErrors({
+      invalidLogin: true
+    });
+  }
 
   get username() {
     return this.form.get('username');
