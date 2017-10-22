d57e3d8cb3b644175ff30ef5170d03b12ab7ff7a
Shock Liang <shockwater@gmail.com>
2017-10-22 16:58:53 +0800

    FormBuilder exercise

--- my-first-app/src/app/new-course-form/new-course-form.component.ts
+++ my-first-app/src/app/new-course-form/new-course-form.component.ts
@@ -1,4 +1,4 @@
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,21 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-course-form.component.css']
 })
 export class NewCourseFormComponent {
-  form = new FormGroup({
-    topics: new FormArray([])
-  });
+  // Regular way to build form.
+  // form = new FormGroup({
+  //   name: new FormControl(),
+  //   contact : new FormGroup({
+  //     email: new FormControl(),
+  //     phone: new FormControl()
+  //   }),
+  //   topics: new FormArray([])
+  // });
+  form;
 
-  addTopic(topic: HTMLInputElement) {
-    this.topics.push(new FormControl(topic.value));
-    topic.value = '';
-  }
-
-  removeTopic(topic: FormControl) {
-    let index = this.topics.controls.indexOf(topic);
-    this.topics.removeAt(index);
-  }
-
-  get topics() {
-    return this.form.get('topics') as FormArray;
+  // FormBuilder way to build form.
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      name: ['', Validators.required],
+      contact: fb.group({
+        email: [],
+        phone: []
+      }),
+      topics: fb.array([])
+    });
   }
 }
