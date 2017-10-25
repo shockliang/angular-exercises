88e75c01461280db56ed84e592bde42420e7d426
Shock Liang <shockwater@gmail.com>
2017-10-25 21:26:52 +0800

    Using OnInit interface for consuming http service

--- my-first-app/src/app/posts/posts.component.ts
+++ my-first-app/src/app/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 
 @Component({
@@ -6,12 +6,16 @@ import { Http } from '@angular/http';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
   posts: any[];
   private url = 'http://jsonplaceholder.typicode.com/posts';
 
   constructor(private http: Http) {
-    http.get(this.url)
+
+  }
+
+  ngOnInit(): void {
+    this.http.get(this.url)
       .subscribe(response => {
         this.posts = response.json();
       })
