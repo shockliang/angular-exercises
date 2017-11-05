df593246b459323216ad214b25b262aec383b34f
Shock Liang <shockwater@gmail.com>
2017-11-05 21:34:58 +0800

    Implementing login

--- auth-demo/src/app/services/auth.service.ts
+++ auth-demo/src/app/services/auth.service.ts
@@ -6,15 +6,26 @@ export class AuthService {
   constructor(private http: Http) {
   }
 
-  login(credentials) { 
-   return this.http.post('/api/authenticate', 
-      JSON.stringify(credentials));
+  login(credentials) {
+    return this.http.post('/api/authenticate',
+      JSON.stringify(credentials))
+      .map(response => {
+        let result = response.json();
+        console.log(result);
+        
+        if (result && result.token) {
+          localStorage.setItem('token', result.token);
+          return true;
+        }
+
+        return false;
+      });
   }
 
-  logout() { 
+  logout() {
   }
 
-  isLoggedIn() { 
+  isLoggedIn() {
     return false;
   }
 }
