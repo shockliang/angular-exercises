65d8456670228d4f4db6541810662ec74d003e2f
Shock Liang <shockwater@gmail.com>
2017-11-06 22:12:27 +0800

    Implementing CanActivate interface to protected route

---
+++ auth-demo/src/app/services/auth-guard.service.ts
@@ -0,0 +1,22 @@
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private router: Router,
+    private authService: AuthService) { }
+
+  canActivate() {
+    if (this.authService.isLoggedIn()) { return true; }
+
+    this.router.navigate(['/login']);
+
+    return false;
+  }
+
+
+}
