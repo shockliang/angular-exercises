9c09c69fd5fe367e26630aac021a69fe88348698
Shock Liang <shockwater@gmail.com>
2017-11-06 22:46:53 +0800

    Accessing protected api resource

--- auth-demo/src/app/services/order.service.ts
+++ auth-demo/src/app/services/order.service.ts
@@ -1,4 +1,4 @@
-import { Http } from '@angular/http';
+import { Http, RequestOptions, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 
@@ -9,7 +9,13 @@ export class OrderService {
   }
 
   getOrders() { 
-    return this.http.get('/api/orders')
+    let headers = new Headers();
+    let token = localStorage.getItem('token');
+    headers.append('Authorization', 'Bearer ' + token);
+
+    let options = new RequestOptions({headers: headers});
+
+    return this.http.get('/api/orders', options)
       .map(response => response.json());
   }
 }
