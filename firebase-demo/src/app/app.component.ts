99d5a8c70a233d3661d8ee00d1013e33b00971d8
Shock Liang <shockwater@gmail.com>
2017-11-09 22:44:09 +0800

    Reading list form firebase

--- firebase-demo/src/app/app.component.ts
+++ firebase-demo/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  courses: any[];
+
+  constructor(db: AngularFireDatabase) {
+    // AngularFire2 4.0 version
+    // db.list('courses')
+    //   .subscribe(courses => {
+    //     this.courses = courses;
+    //     console.log(this.courses);
+    //   })
+    
+    // AngularFire2 5.0 version
+    db.list('courses')
+      .valueChanges()
+      .subscribe(courses => {
+        this.courses = courses;
+        console.log(this.courses);
+      });
+  }
 }
