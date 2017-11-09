b6b63cdb130299c980710ef08640f1faa55bf9e7
Shock Liang <shockwater@gmail.com>
2017-11-09 22:57:06 +0800

    Using unsubscribe to prevent memory leaks when using firebase database

--- firebase-demo/src/app/app.component.ts
+++ firebase-demo/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy{
   courses: any[];
+  subscription: Subscription;
 
   constructor(db: AngularFireDatabase) {
     // AngularFire2 4.0 version
@@ -19,11 +22,15 @@ export class AppComponent {
     //   })
 
     // AngularFire2 5.0 version
-    db.list('courses')
+    this.subscription = db.list('courses')
       .valueChanges()
       .subscribe(courses => {
         this.courses = courses;
         console.log(this.courses);
       });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
