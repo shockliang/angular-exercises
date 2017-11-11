2302081f1c5cbda76800948f586811ff17bea99d
Shock Liang <shockwater@gmail.com>
2017-11-12 00:55:41 +0800

    Updating an object using set method

--- firebase-demo/src/app/app.component.ts
+++ firebase-demo/src/app/app.component.ts
@@ -12,24 +12,22 @@ export class AppComponent {
   coursesRef: AngularFireList<any>;
   courses$: Observable<any[]>;
 
-  constructor(db: AngularFireDatabase) {
+  constructor(private db: AngularFireDatabase) {
     this.coursesRef = db.list('courses');
-    this.courses$ = this.coursesRef.valueChanges();
+    this.courses$ = this.coursesRef.snapshotChanges()
+      .map(changes => {
+        return changes.map(c => ({ key: c.payload.key, value: c.payload.val() }));
+      });
 
   }
 
   add(course: HTMLInputElement) {
-    // this.coursesRef.push(course.value);
-    this.coursesRef.push({
-      name: course.value,
-      price: 150,
-      isLive: true,
-      sections: [
-        { title: 'Components' },
-        { title: 'Directives' },
-        { title: 'Template' },
-      ]
-    })
+    this.coursesRef.push(course.value);
     course.value = '';
+  }
+
+  update(course) {
+    console.log(course);
+    this.coursesRef.set(course.key, course.value + ' UPDATED');
   }
 }
