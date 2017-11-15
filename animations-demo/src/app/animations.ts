edb11fe7cbd1140374c637a7390f44d335f69f34
Shock Liang <shockwater@gmail.com>
2017-11-15 21:03:24 +0800

    Keyframes exercise

--- animations-demo/src/app/animations.ts
+++ animations-demo/src/app/animations.ts
@@ -1,20 +1,32 @@
-import { trigger, state, style, transition, animate } from "@angular/animations";
+import { trigger, state, style, transition, animate, keyframes } from "@angular/animations";
 
 export let fade = trigger('fade', [
-    state('void', style({ opacity: 0 })),
+  state('void', style({ opacity: 0 })),
 
-    transition(':enter, :leave', [  // void <=> *
-        animate(500)
-    ]),
+  transition(':enter, :leave', [  // void <=> *
+    animate(500)
+  ]),
 ]);
 
 export let slide = trigger('slide', [
-    transition(':enter', [
-        style({ transform: 'translateX(-10px)' }),
-        animate(250),
-    ]),
+  transition(':enter', [
+    style({ transform: 'translateX(-10px)' }),
+    animate(250),
+  ]),
 
-    transition(':leave', [
-        animate('0.5s cubic-bezier(.15,.91,1,.63)', style({ transform: 'translateX(-100%)' }))
-    ]),
+  transition(':leave', [
+    animate('0.5s ease-out', keyframes([
+      style({
+        offset: .2,
+        opacity: 1,
+        transform: 'translateX(20px)'
+      }),
+      style({
+        offset: 1,
+        opacity: 0,
+        transform: 'translateX(-100%)'
+      }),
+    ])
+    )
+  ]),
 ]);
