da99f30c83af893093d3a0cd5af5be9bbff5e187
Shock Liang <shockwater@gmail.com>
2017-11-17 23:50:59 +0800

    Multi-step animations

--- animations-demo/src/app/zippy/zippy.component.ts
+++ animations-demo/src/app/zippy/zippy.component.ts
@@ -11,11 +11,16 @@ import { trigger, state, style, transition, animate, keyframes, useAnimation, qu
         height: 0,
         paddingTop: 0,
         paddingBottom: 0,
-        overflow: 'hidden',
+        opacity: 0
       })),
 
       transition('collapsed => expanded', [
-        animate('300ms ease-out')
+        animate('300ms ease-out', style({
+          height: '*',
+          paddingTop: '*',
+          paddingBottom: '*'
+        })),
+        animate('1s', style({opacity: 1}))
       ]),
       transition('expanded => collapsed', [
         animate('300ms ease-in')
