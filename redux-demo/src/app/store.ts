5cb86b1dbfec6847f209eab02a95e59717bc9501
Shock Liang <shockwater@gmail.com>
2017-11-22 23:20:01 +0800

    Avoiding state mutation

--- redux-demo/src/app/store.ts
+++ redux-demo/src/app/store.ts
@@ -1,5 +1,6 @@
 import { IAppState } from './store';
 import { INCREMENT } from "./actions";
+import { tassign } from 'tassign'
 
 export const INITIAL_STATE: IAppState = {
   counter: 0
@@ -11,7 +12,9 @@ export interface IAppState {
 
 export function rootReducer(state: IAppState, action): IAppState {
   switch (action.type) {
-    case INCREMENT: return { counter: state.counter + 1 }
+    case INCREMENT:
+      // return Object.assign({}, state, { counter: state.counter + 1 });
+      return tassign(state, { counter: state.counter + 1 });
   }
   return state;
 }
