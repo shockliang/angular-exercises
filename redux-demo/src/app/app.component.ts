1c54d78e9f65e196f5c02a0b828abe7956119944
Shock Liang <shockwater@gmail.com>
2017-11-23 22:52:14 +0800

    Redux exercises

--- redux-demo/src/app/app.component.ts
+++ redux-demo/src/app/app.component.ts
@@ -1,23 +1,10 @@
-import { IAppState } from './store';
-import { NgRedux, select } from '@angular-redux/store';
 import { Component } from '@angular/core';
-import { INCREMENT } from './actions';
-import { Map } from 'immutable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  title = 'app';
-  @select(s => s.get('counter')) count;
-
-  constructor(private ngRedux: NgRedux<Map<string, any>>) {
-  }
-
-  increment() {
-    this.ngRedux.dispatch({ type: INCREMENT });
-  }
+  title = 'app works!';
 }
--- redux-demo/src/app/app.module.ts
+++ redux-demo/src/app/app.module.ts
@@ -1,24 +1,31 @@
-import { IAppState, rootReducer, INITIAL_STATE } from './store';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { NgRedux, NgReduxModule } from '@angular-redux/store';
-import { fromJS, Map } from 'immutable';
 
 import { AppComponent } from './app.component';
+import { IAppState, rootReducer, INITIAL_STATE } from './store';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoDashboardComponent } from './todo-dashboard/todo-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoDashboardComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     NgReduxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(ngRedux: NgRedux<Map<string, any>>) {
-    ngRedux.configureStore(rootReducer, fromJS(INITIAL_STATE));
+  constructor(ngRedux: NgRedux<IAppState>){
+    ngRedux.configureStore(rootReducer, INITIAL_STATE);
   }
-}
+ }
