de840907fe16ab1a4d7fe481f74d61dca4d24209
Shock Liang <shockwater@gmail.com>
2017-11-25 15:20:41 +0800

    Set up and tear down.

--- testing-demo/src/app/03-setup-and-teardown/vote.component.spec.ts
+++ testing-demo/src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,22 @@
 import { VoteComponent } from './vote.component'; 
 
 describe('VoteComponent', () => {
-  it('', () => {
+  let component: VoteComponent;
+
+  beforeEach(()=> {
+    component = new VoteComponent();
+  });
+  
+  it('should increment totalVotes when upVoted', () => {
+    component.upVote();
+    
+    expect(component.totalVotes).toBe(1);
   });
 
-  it('', () => {
+  it('should decrement totalVotes when downVotes', () => {
+    component.downVote();
+    
+    expect(component.totalVotes).toBe(-1);
   });
+
 });
