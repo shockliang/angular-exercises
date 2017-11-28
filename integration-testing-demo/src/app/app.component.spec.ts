4c16a1380c11abb24850cc05085c7ee71b4f3a05
Shock Liang <shockwater@gmail.com>
2017-11-28 20:58:32 +0800

    Testing RouterOutlet components

--- integration-testing-demo/src/app/app.component.spec.ts
+++ integration-testing-demo/src/app/app.component.spec.ts
@@ -5,6 +5,8 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -12,6 +14,7 @@ describe('AppComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ RouterTestingModule.withRoutes([]) ],
       declarations: [ AppComponent ]
     })
     .compileComponents();
@@ -21,5 +24,19 @@ describe('AppComponent', () => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
     fixture.detectChanges(); 
+  });
+
+  it('should have a router outlet', () => {
+    let de = fixture.debugElement.query(By.directive(RouterOutlet));
+
+    expect(de).not.toBeNull();
+  });
+
+  it('should have a link to todos page', () => {
+    let debugElements = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
+
+    let index = debugElements.findIndex(de => de.properties['href'] === '/todos');
+
+    expect(index).toBeGreaterThan(-1);
   });
 });
