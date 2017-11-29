d817d8a1f3a00fd8c1f822f771cf9ec945e7dbd7
Shock Liang <shockwater@gmail.com>
2017-11-29 20:09:13 +0800

    Install new packages for project

--- oshop/src/app/app.module.ts
+++ oshop/src/app/app.module.ts
@@ -1,5 +1,10 @@
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +13,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- oshop/src/environments/environment.prod.ts
+++ oshop/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "fds;fsaffdsfjsdfpoufidsojf;saf",
+    authDomain: "oshop-28dd4.firebaseapp.com",
+    databaseURL: "https://oshop-28dd4.firebaseio.com",
+    projectId: "oshop-28dd4",
+    storageBucket: "",
+    messagingSenderId: "938763989047"
+  }
 };
