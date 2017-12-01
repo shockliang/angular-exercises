3fbc5ff97100e32ef2f933275e242769705de07a
Shock Liang <shockwater@gmail.com>
2017-12-02 00:25:46 +0800

    Extracting a service response for auth implement

--- oshop/src/app/app.module.ts
+++ oshop/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { OrderSuccessComponent } from './order-success/order-success.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
@@ -51,7 +52,9 @@ import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.componen
       { path: 'admin/orders', component: AdminOrdersComponent },
     ])
   ],
-  providers: [],
+  providers: [
+    AuthService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
