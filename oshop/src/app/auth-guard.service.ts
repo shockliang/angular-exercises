588c92b6cbaf610e421520879efa0a1c24b3ddaa
Shock Liang <shockwater@gmail.com>
2017-12-03 00:25:54 +0800

    Redirecting after loggin in

--- oshop/src/app/auth-guard.service.ts
+++ oshop/src/app/auth-guard.service.ts
@@ -1,7 +1,7 @@
 import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router/src/interfaces';
-import { Router } from '@angular/router';
+import { Router, RouterStateSnapshot } from '@angular/router';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -9,11 +9,11 @@ export class AuthGuard implements CanActivate {
 
   constructor(private auth: AuthService, private router: Router) { }
 
-  canActivate() {
+  canActivate(route, state: RouterStateSnapshot) {
     return this.auth.user$.map(user => {
       if (user) return true;
 
-      this.router.navigate(['/login']);
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
       return false;
     })
   }
