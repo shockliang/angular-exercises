f91d5270d66c73e0b7bf66e04f7c638a443b1896
Shock Liang <shockwater@gmail.com>
2017-12-05 00:34:26 +0800

    Fixng weird bug... That has double spacesbefore 'admin/products' after path:

--- oshop/src/app/app.module.ts
+++ oshop/src/app/app.module.ts
@@ -52,8 +52,8 @@ import { AdminAuthGuard } from './admin-auth-guard.service';
       { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuard] },
       { path: 'order-success', component: OrderSuccessComponent, canActivate: [AuthGuard] },
       { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard] },
-      { path: 'admin/orders', component: AdminOrdersComponent, canActivate: [AuthGuard, AdminAuthGuard] },
-      { path: 'admin/products', component: AdminProductsComponent, canActivate: [AuthGuard, AdminAuthGuard] },
+      { path: 'admin/orders', component: AdminOrdersComponent, canActivate: [AuthGuard, AdminAuthGuard] },
+      { path: 'admin/products', component: AdminProductsComponent, canActivate: [AuthGuard, AdminAuthGuard] },
     ])
   ],
   providers: [
