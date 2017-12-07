4d081b536f2846c41d52c08f5409b1f93262179a
Shock Liang <shockwater@gmail.com>
2017-12-07 21:04:22 +0800

    Searching for products in the client side

--- oshop/src/app/admin/admin-products/admin-products.component.ts
+++ oshop/src/app/admin/admin-products/admin-products.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../product.service';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: { title: string }[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
-  constructor(private productService: ProductService) { 
-    this.products$ = this.productService.getAll();
+  constructor(private productService: ProductService) {
+    this.subscription = this.productService.getAll()
+      .subscribe(products => this.filteredProducts = this.products = products);
+  }
+
+  filter(query: string) {
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   ngOnInit() {
   }
+
 
 }
