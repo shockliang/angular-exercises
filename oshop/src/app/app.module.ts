ec956b2ca1c5fb7f774163f6f072806571c92345
Shock Liang <shockwater@gmail.com>
2017-12-08 21:19:49 +0800

    Refactoring: Extracting the ProductCardComponent

--- oshop/src/app/app.module.ts
+++ oshop/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { ProductFormComponent } from './admin/product-form/product-form.componen
 import { CategoryService } from './category.service';
 import { ProductService } from './product.service';
 import { ProductFilterComponent } from './products/product-filter/product-filter.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,8 @@ import { ProductFilterComponent } from './products/product-filter/product-filter
     AdminOrdersComponent,
     LoginComponent,
     ProductFormComponent,
-    ProductFilterComponent
+    ProductFilterComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
