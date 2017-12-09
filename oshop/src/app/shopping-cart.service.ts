e1815be8c0523e26e8b9fe87529d789d2f2ce0eb
Shock Liang <shockwater@gmail.com>
2017-12-09 22:57:17 +0800

    Adding a product to the shopping cart

--- oshop/src/app/shopping-cart.service.ts
+++ oshop/src/app/shopping-cart.service.ts
@@ -1,5 +1,7 @@
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
+import { Product } from './models/product';
+import 'rxjs/add/operator/take';
 
 @Injectable()
 export class ShoppingCartService {
@@ -16,14 +18,21 @@ export class ShoppingCartService {
     return this.db.object('/shopping-carts' + cartId);
   }
 
-  private async getOrCreateCart() {
+  private async getOrCreateCartId() {
     let cartId = localStorage.getItem('cartId');
-    if (!cartId) {
-      let result = await this.create();
-      localStorage.setItem('cartId', result.key);
-      return this.getCart(result.key);
-    }
-    
-    return this.getCart(cartId);
+    if (cartId) return cartId;
+
+    let result = await this.create();
+    localStorage.setItem('cartId', result.key);
+    return result.key;
+  }
+
+  async addToCart(product: Product) {
+    let cartId = await this.getOrCreateCartId();
+    let item$ = this.db.object('/shopping-carts/' + cartId + '/items/' + product.$key);
+    item$.take(1).subscribe(item => {
+      if (item.$exists()) item$.update({ quantity: item.quantity + 1 });
+      else item$.set({ product: product, quantity: 1 });
+    })
   }
 }
