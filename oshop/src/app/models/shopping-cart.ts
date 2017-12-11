bcd7cc5048501ffea072d0854f5f7f72ac944a04
Shock Liang <shockwater@gmail.com>
2017-12-11 20:36:04 +0800

    Refactoring: Extracting ProductQuantityComponents

--- oshop/src/app/models/shopping-cart.ts
+++ oshop/src/app/models/shopping-cart.ts
@@ -1,4 +1,5 @@
 import { ShoppingCartItem } from './shopping-cart-item';
+import { Product } from './product';
 
 export class ShoppingCart {
 
@@ -9,6 +10,11 @@ export class ShoppingCart {
       let item = itemsMap[productId];
       this.items.push(new ShoppingCartItem(item.product, item.quantity));
     }
+  }
+
+  getQuantity(product: Product) {
+    let item = this.itemsMap[product.$key];
+    return item ? item.quantity : 0;
   }
 
   get totalPrice() {
