814e9c93023b1f47327cd5ca4b4ef5627d3eb39c
Shock Liang <shockwater@gmail.com>
2017-12-12 21:08:25 +0800

    Saving the order in firebase

--- oshop/src/app/check-out/check-out.component.ts
+++ oshop/src/app/check-out/check-out.component.ts
@@ -1,14 +1,52 @@
+import { OrderService } from './../order.service';
+import { Subscription } from 'rxjs/Subscription';
+import { ShoppingCart } from './../models/shopping-cart';
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent { 
-  shipping = {}; 
-  
+export class CheckOutComponent implements OnInit, OnDestroy {
+  shipping = {};
+  cart: ShoppingCart;
+  subscription: Subscription;
+
+  constructor(
+    private shoppingCartService: ShoppingCartService,
+    private orderService: OrderService) {
+
+  }
+
+  async ngOnInit() {
+    let cart$ = await this.shoppingCartService.getCart();
+    this.subscription = cart$.subscribe(cart => this.cart = cart);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
   placeOrder() {
-    console.log(this.shipping);
-  }    
+    let order = {
+      datePlaced: new Date().getTime(),
+      shipping: this.shipping,
+      items: this.cart.items.map(i=> {
+        return {
+          product: {
+            title: i.title,
+            imageUrl: i.imageUrl,
+            price: i.price
+          },
+          quantity: i.quantity,
+          totalPrice: i.totalPrice
+        }
+      })
+    }
+
+    this.orderService.storeOrder(order);
+  }
 }
