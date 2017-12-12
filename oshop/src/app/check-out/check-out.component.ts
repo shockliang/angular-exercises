de5c05bc5425778bb22ad2708900e23c4f341f25
Shock Liang <shockwater@gmail.com>
2017-12-12 21:14:24 +0800

    Associating the order with current user

--- oshop/src/app/check-out/check-out.component.ts
+++ oshop/src/app/check-out/check-out.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../auth.service';
 import { OrderService } from './../order.service';
 import { Subscription } from 'rxjs/Subscription';
 import { ShoppingCart } from './../models/shopping-cart';
@@ -13,28 +14,33 @@ import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 export class CheckOutComponent implements OnInit, OnDestroy {
   shipping = {};
   cart: ShoppingCart;
-  subscription: Subscription;
+  userId: string;
+  cartSubscription: Subscription;
+  userSubscription: Subscription;
 
   constructor(
     private shoppingCartService: ShoppingCartService,
-    private orderService: OrderService) {
-
-  }
+    private orderService: OrderService,
+    private authService: AuthService) {
+    }
 
   async ngOnInit() {
     let cart$ = await this.shoppingCartService.getCart();
-    this.subscription = cart$.subscribe(cart => this.cart = cart);
+    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
+    this.userSubscription = this.authService.user$.subscribe(user => this.userId = user.uid);
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.cartSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
   }
 
   placeOrder() {
     let order = {
+      userId:this.userId,
       datePlaced: new Date().getTime(),
       shipping: this.shipping,
-      items: this.cart.items.map(i=> {
+      items: this.cart.items.map(i => {
         return {
           product: {
             title: i.title,
