3cbf1b05130754b32683e2cd4c935b20cf50c015
Shock Liang <shockwater@gmail.com>
2017-12-15 23:14:38 +0800

    Moving admin-orders into admin/components folder

--- oshop/src/app/app.module.ts
+++ oshop/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { CustomFormsModule } from 'ng2-validation';
 
 import { environment } from './../environments/environment';
 import { AdminAuthGuard } from './admin-auth-guard.service';
-import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AdminOrdersComponent } from './admin/components/admin-orders/admin-orders.component';
 import { AdminProductsComponent } from './admin/components/admin-products/admin-products.component';
 import { ProductFormComponent } from './admin/components/product-form/product-form.component';
 import { AppComponent } from './app.component';
