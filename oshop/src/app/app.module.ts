5855dcc808a54aa4965dcd7129cf6663e2ef796b
Shock Liang <shockwater@gmail.com>
2017-12-15 23:11:52 +0800

    Moving product-form into admin/component folder

--- oshop/src/app/app.module.ts
+++ oshop/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { environment } from './../environments/environment';
 import { AdminAuthGuard } from './admin-auth-guard.service';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
-import { ProductFormComponent } from './admin/product-form/product-form.component';
+import { ProductFormComponent } from './admin/components/product-form/product-form.component';
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 import { CheckOutComponent } from './check-out/check-out.component';
